0984bf1c73689aef2b295a2f6719acac2b8770ed
pineapplesvob <danielkstaehr@gmail.com>
2024-03-12 09:59:46 +0100

    Player selection

---
+++ Assets/Scripts/PlayerSelection.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerSelection : MonoBehaviour
+{
+    public void On2Players()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void On3Players ()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void On4Players ()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
