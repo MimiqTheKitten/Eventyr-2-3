740bb03d35de41e558d65a23d463a039b39e314d
MimiqTheKitten <117112178+MimiqTheKitten@users.noreply.github.com>
2024-05-07 09:06:07 +0200

    added player select to main menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,9 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-  public void OnPlayButton ()
+  public void OnPlayButton (int loadscene)
     {
-        SceneManager.LoadScene(1); //det behøver egentlig ikke være 1 den er sat til, det er bare vigtigt at den scene vi gerne vil ændre til's 'ID' inde på build settings er det samme som det der står her
+        SceneManager.LoadScene(loadscene); //det behøver egentlig ikke være 1 den er sat til, det er bare vigtigt at den scene vi gerne vil ændre til's 'ID' inde pEbuild settings er det samme som det der står her
     }
     public void OnQuitButton ()
     {
