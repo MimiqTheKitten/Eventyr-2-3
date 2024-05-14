490387fbfb5f3e6c4e6a0d3413e7a8c3b23c60d7
MimiqTheKitten <117112178+MimiqTheKitten@users.noreply.github.com>
2024-05-14 10:14:42 +0200

    changed and added sounds

--- Assets/Scripts/WinManager.cs
+++ Assets/Scripts/WinManager.cs
@@ -1,11 +1,14 @@
 using UnityEngine.SceneManagement;
 using UnityEngine;
 using System.Linq;
+using System.Threading;
 
 public class WinManager : MonoBehaviour
 {
     GameObject[] players;
     [SerializeField] int loadScene = 0;
+    float timer = 3f;
+    bool doTimer;
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +20,21 @@ public class WinManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (doTimer)
+        {
+            timer -= Time.deltaTime;
+        }
         players = GameObject.FindGameObjectsWithTag("Player");
         if(players.Length <= 1)
         {
+            FindObjectOfType<AudioManager>().play("WinSound");
             Debug.Log("Winner found");
-            SceneManager.LoadScene(loadScene);            
+            
+                 
+        }
+        if(timer <= 0)
+        {
+            SceneManager.LoadScene(loadScene);
         }
 
     }
